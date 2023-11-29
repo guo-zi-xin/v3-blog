da1414e0aa906430b53a8d82fc3a2ca490bfc146
guo-zi-xin <1992059868@qq.com>
2023-11-29 19:44:16 +0800

    feat(components): layout布局组件功能开发

---
+++ blog-front/src/components/layouts/BasicLayout.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import Header from '@/components/layouts/header/Header.vue'
+import Main from '@/components/layouts/main/Main.vue'
+</script>
+
+<template>
+  <n-layout wh-full>
+    <n-layout-header>
+      <Header />
+    </n-layout-header>
+    <n-layout-content content-style="padding: 24px;" wh-full>
+      <Main mt30/>
+    </n-layout-content>
+    <n-layout-footer>footer</n-layout-footer>
+  </n-layout>
+</template>
+
+<style lang="scss" scoped>
+.n-layout-header {
+  width: 100%;
+}
+
+.n-layout-footer {
+  padding: 24px;
+}
+
+.n-layout-content {
+  // background-color: #fff;
+  overflow: auto;
+}
+</style>
